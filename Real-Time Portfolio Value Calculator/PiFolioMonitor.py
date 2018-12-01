85bc370cd1210364d531ade95ae369701065d6ea
Thomas Burgess <thomasburgess2000@gmail.com>
2018-12-01 14:18:37 -0800

    Primariliy readability changes

--- Real-Time Portfolio Value Calculator/PiFolioMonitor.py
+++ Real-Time Portfolio Value Calculator/PiFolioMonitor.py
@@ -7,69 +7,64 @@ from sense_hat import SenseHat
 
 #Variables for calculation
 currentDT = dt.now()
-currentYMD= str(currentDT.year) + '-' + str(currentDT.month) + '-' + str(currentDT.day)
-delta=timedelta(days=1)
-yesterdayUnformatted=currentDT-delta
-yesterdayYMD=yesterdayUnformatted.strftime('%Y-%m-%d')
+currentYMD = str(currentDT.year) + '-' + str(currentDT.month) + '-' + str(currentDT.day)
+delta = timedelta(days = 1)
+yesterdayUnformatted = currentDT-delta
+yesterdayYMD = yesterdayUnformatted.strftime('%Y-%m-%d')
+yestderdayClose = yesterdayYMD+' '+'15:30:00'
 currentHour = currentDT.hour
-currentHourIndex=currentDT.hour-9
-times=[currentYMD+' '+'09:30:00',currentYMD+' '+'10:30:00',currentYMD+' '+'11:30:00',currentYMD+' '+'12:30:00',currentYMD+' '+'13:30:00',currentYMD+' '+'14:30:00',currentYMD+' '+'15:30:00']
-portfolioValues=[]
+currentHourIndex = currentDT.hour-9
+times = [currentYMD+' '+'09:30:00',currentYMD+' '+'10:30:00',currentYMD+' '+'11:30:00',currentYMD+' '+'12:30:00',currentYMD+' '+'13:30:00',currentYMD+' '+'14:30:00',currentYMD+' '+'15:30:00']
+portfolioValues = []
 
 #Variables for Sense HAT
 sense = SenseHat()
-yellow= (255,255,0)
-red=(255,0,0)
-green=(0,255,0)
+yellow = (255,255,0)
+red = (255,0,0)
+green = (0,255,0)
 
 #Portfolio ['SYMBOL',# of shares]
-portfolio=[['TSLA',16],['OLED',6],['NTDOY',2]]
+portfolio = [['TSLA',16],['OLED',6],['NTDOY',2]]
 
 #Takes a symbol and time as input, prints that hour's price, and returns that hour's price
 def stockPrice(symbol, time):
-    ts = TimeSeries(key='WJOFY5CTSKEC2VSP')
-    data = ts.get_intraday(symbol=symbol,interval='60min', outputsize='compact')
-    print(data[0][time]['4. close'])
-    return (float((data[0][time]['4. close'])))
-
-#Runs stockPrice as many hours as the stock market has been open today
-def getHourly():
-    startTime=9
-    hourIndex=0
-    while ((startTime<currentDT.hour) and (startTime<16)):
-       # stockPrice(ticker,times[hourIndex])
-        hourIndex+=1
-        startTime+=1
+    ts = TimeSeries(key = 'WJOFY5CTSKEC2VSP')
+    data = ts.get_intraday(symbol = symbol, interval = '60min', outputsize = 'compact')
+    varStockPrice = data[0][time]['1. open']
+    print(varStockPrice)
+    return (float(varStockPrice))
 
 #Takes ticker symbol, number of stocks you have, and current stock price to calculate how much money you have in a certain stock
 #I'm pretty sure currentHour-9 equals the index of the times variable
 def stockCalc(symbol,shares,time):
-    price=stockPrice(symbol,time)
+    price = stockPrice(symbol,time)
     return (price*shares)
 
-#Calculates the current value of your portfolio when run
+#Calculates the value of your portfolio at time when run. The format of time should be "Y-M-D HOUR-MIN-SEC"
 def portCalc(time):
-    portfolioValue=0
-    portfolioIteration=0
+    portfolioValue = 0
+    portfolioIteration = 0
     #calculate the value of each ticker I have (price * shares)
     while (portfolioIteration<(len(portfolio))):
-        print ("Checking "+ portfolio[portfolioIteration][0])
-        portfolioValue=portfolioValue+ (stockCalc(portfolio[portfolioIteration][0],portfolio[portfolioIteration][1],time))
-        portfolioIteration+=1
+        currentSymbol = portfolio[portfolioIteration][0]
+        currentShares = portfolio[portfolioIteration][1]
+        print ("Checking " + currentSymbol)
+        portfolioValue = portfolioValue+ (stockCalc(currentSymbol, currentShares, time))
+        portfolioIteration += 1
     print (portfolioValue)
     return portfolioValue
 
 #Takes past portfolio values and changes lights accordingly
 def lightChange():
-    startTime=9
-    hourIndex=0
+    startTime = 9
+    hourIndex = 0
     #puts portfolio values for each hour into the portfolioValues list
     while ((startTime<currentDT.hour) and (startTime<16)):
-        portfolioValues[hourIndex]=portCalc(times[hourIndex])
-        hourIndex+=1
-        startTime+=1
+        portfolioValues[hourIndex] = portCalc(times[hourIndex])
+        hourIndex += 1
+        startTime += 1
     #while loop that runs through number of indexes in portfolioValues and changes lights accordingly (needs to compare to starting value somehow)
-
+    startingValue = portCalc(yesterdayYMD)
 
 portCalc(times[0])
 
