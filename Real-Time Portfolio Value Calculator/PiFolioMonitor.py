8a23f42b0f1f71d8827d7f5182ad103a89c8cbc4
Thomas Burgess <thomasburgess2000@gmail.com>
2018-12-01 16:54:18 -0800

    Final major changes to core program before debugging on Pi

--- Real-Time Portfolio Value Calculator/PiFolioMonitor.py
+++ Real-Time Portfolio Value Calculator/PiFolioMonitor.py
@@ -8,14 +8,18 @@ from sense_hat import SenseHat
 #Variables for calculation
 currentDT = dt.now()
 currentYMD = str(currentDT.year) + '-' + str(currentDT.month) + '-' + str(currentDT.day)
+
 delta = timedelta(days = 1)
 yesterdayUnformatted = currentDT-delta
 yesterdayYMD = yesterdayUnformatted.strftime('%Y-%m-%d')
-yestderdayClose = yesterdayYMD+' '+'15:30:00'
+yesterdayClose = yesterdayYMD + ' ' + '15:30:00'
+
 currentHour = currentDT.hour
 currentHourIndex = currentDT.hour-9
-times = [currentYMD+' '+'09:30:00',currentYMD+' '+'10:30:00',currentYMD+' '+'11:30:00',currentYMD+' '+'12:30:00',currentYMD+' '+'13:30:00',currentYMD+' '+'14:30:00',currentYMD+' '+'15:30:00']
+times = [currentYMD+' '+'09:30:00', currentYMD+' '+'10:30:00', currentYMD+' '+'11:30:00', currentYMD+' '+'12:30:00', currentYMD+' '+'13:30:00', currentYMD+' '+'14:30:00', currentYMD+' '+'15:30:00']
+
 portfolioValues = []
+portfolioChanges = []
 
 #Variables for Sense HAT
 sense = SenseHat()
@@ -27,12 +31,19 @@ green = (0,255,0)
 portfolio = [['TSLA',16],['OLED',6],['NTDOY',2]]
 
 #Takes a symbol and time as input, prints that hour's price, and returns that hour's price
+#I need to make this so that when 15:30:00 is input for column 6 it gets the open value, but when it is after 16:30:00
 def stockPrice(symbol, time):
     ts = TimeSeries(key = 'WJOFY5CTSKEC2VSP')
     data = ts.get_intraday(symbol = symbol, interval = '60min', outputsize = 'compact')
-    varStockPrice = data[0][time]['1. open']
+    if ((currentDT.hour==16 and currentDT.minute>29) or (currentDT.hour>16) or (time==yesterdayClose)):
+        if ('15:30:00' in time):
+            varStockPrice=data[0][time]['4. close']
+    else:
+        varStockPrice = data[0][time]['1. open']
     print(varStockPrice)
     return (float(varStockPrice))
+
+
 
 #Takes ticker symbol, number of stocks you have, and current stock price to calculate how much money you have in a certain stock
 #I'm pretty sure currentHour-9 equals the index of the times variable
@@ -59,12 +70,144 @@ def lightChange():
     startTime = 9
     hourIndex = 0
     #puts portfolio values for each hour into the portfolioValues list
-    while ((startTime<currentDT.hour) and (startTime<16)):
-        portfolioValues[hourIndex] = portCalc(times[hourIndex])
-        hourIndex += 1
-        startTime += 1
-    #while loop that runs through number of indexes in portfolioValues and changes lights accordingly (needs to compare to starting value somehow)
-    startingValue = portCalc(yesterdayYMD)
+    if ((currentDT.weekday()==6) or (currentDT.weekday()==5)):
+        print ("Markets closed for weekend")
+        sense.show_message("Markets closed for weekend")
+    else:
+        while ((startTime<(currentDT.hour+1)) and (startTime<16)):
+            portfolioValues[hourIndex] = portCalc(times[hourIndex])
+            hourIndex += 1
+            startTime += 1
+#Puts percentage changes (portfolioValues vs startingValue) into portfolioChanges array
+        hourIndex=0
+        startingValue = portCalc(yesterdayClose)
+        while (hourIndex<(len(portfolioValues))):
+            portfolioChanges[hourIndex] = ((portfolioValues[hourIndex]-startingValue)/startingValue)*100
+            print (portfolioChanges[hourIndex])
+            hourIndex += 1
+    #while loop that runs through number of indexes in portfolioChanges and changes lights accordingly (hourIndex is column)
+        hourIndex=0
+        while (hourIndex<(len(portfolioChanges))):
+            #For when the final displayed value is between -3.5% and 3.5%
+            finalDisplayedChange = portfolioChanges[(len(portfolioChanges)-1)]
+            currentChange=portfolioChanges[hourIndex]
+            if (finalDisplayedChange<3.5 and finalDisplayedChange>-3.5):
+                #For value changes between -3.5% and 3.5%
+                if (currentChange<3.5 and currentChange>-3.5):
+                    #Row 0
+                    if (currentChange>2.5 and currentChange<3.5):
+                        sense.set_pixel(hourIndex,0,yellow)
+                    #Row 1
+                    elif (currentChange>1.5 and currentChange<2.5):
+                        sense.set_pixel(hourIndex,1,yellow)
+                    #Row 2
+                    elif (currentChange>0.5 and currentChange<1.5):
+                        sense.set_pixel(hourIndex,2,yellow)
+                    #Row 3
+                    elif (currentChange>0 and currentChange<0.5):
+                        sense.set_pixel(hourIndex,3,yellow)
+                    #Row 4
+                    elif (currentChange>-0.5 and currentChange<0):
+                        sense.set_pixel(hourIndex,4,yellow)
+                    #Row 5
+                    elif (currentChange>-1.5 and currentChange<-0.5):
+                        sense.set_pixel(hourIndex,5,yellow)
+                    #Row 6
+                    elif (currentChange>-2.5 and currentChange<-1.5):
+                        sense.set_pixel(hourIndex,6,yellow)
+                    #Row 7
+                    elif (currentChange>-3.5 and currentChange<-2.5):
+                        sense.set_pixel(hourIndex,7,yellow)
+                #For value changes between 3.5% and 11.5%
+                elif (currentChange>3.5 and currentChange<11.5):
+                    sense.set_pixel(hourIndex,0,green)
+                #For value changes between -3.5% and -11.5%
+                elif (currentChange<-3.5 and currentChange>-11.5):
+                    sense.set_pixel(hourIndex,7,red)
+                #For values changes greater than 11.5% in either direction
+                else:
+                    print (currentChange)
+                    sense.show_message(str(currentChange))
+            #For when the final displayed value is between 3.5% and 11.5%
+            elif (finalDisplayedChange>3.5 and finalDisplayedChange<11.5):
+                #For value changes between -3.5% and 3.5%
+                if (currentChange<3.5 and currentChange>-3.5):
+                    sense.set_pixel(hourIndex, 7, yellow)
+                #For value changes between -3.5% and -11.5%
+                elif (currentChange<-3.5 and currentChange>-11.5):
+                    sense.set_pixel(hourIndex, 7, red)
+                #For value changes between 3.5% and 11.5%
+                elif (currentChange>3.5 and currentChange<11.5):
+                    #Row 7
+                    if (currentChange>3.5 and currentChange<4.5):
+                        sense.set_pixel(hourIndex,7,green)
+                    #Row 6
+                    elif (currentChange>4.5 and currentChange<5.5):
+                        sense.set_pixel(hourIndex,6,green)
+                    #Row 5
+                    elif (currentChange>5.5 and currentChange<6.5):
+                        sense.set_pixel(hourIndex,5,green)
+                    #Row 4
+                    elif (currentChange>6.5 and currentChange<7.5):
+                        sense.set_pixel(hourIndex,4,green)
+                    #Row 3
+                    elif (currentChange>7.5 and currentChange<8.5):
+                        sense.set_pixel(hourIndex,3,green)
+                    #Row 2
+                    elif (currentChange>8.5 and currentChange<9.5):
+                        sense.set_pixel(hourIndex,2,green)
+                    #Row 1
+                    elif (currentChange>9.5 and currentChange<10.5):
+                        sense.set_pixel(hourIndex,1,green)
+                    #Row 0
+                    elif (currentChange>10.5 and currentChange<11.5):
+                        sense.set_pixel(hourIndex,0,green)
+                else:
+                    print (currentChange)
+                    sense.show_message(str(currentChange))
+            #For when the final displayed value is between -3.5% and -11.5%
+            elif (finalDisplayedChange>-11.5 and finalDisplayedChange<-3.5):
+                #For value changes between -3.5% and 3.5%
+                if (currentChange>-3.5 and currentChange<3.5):
+                    sense.set_pixel(hourIndex,0,yellow)
+                #For value changes between 3.5% and 11.5%
+                elif (currentChange>3.5 and currentChange<11.5):
+                    sense.set_pixel(hourIndex,0,green)
+                #For value changes between -3.5% and -11.5%
+                elif (currentChange<-3.5 and currentChange>-11.5):
+                    #Row 0
+                    if (currentChange<-3.5 and currentChange>-4.5):
+                        sense.set_pixel(hourIndex,0,red)
+                    #Row 1
+                    elif (currentChange<-4.5 and currentChange>-5.5):
+                        sense.set_pixel(hourIndex,1,red)
+                    #Row 2
+                    elif (currentChange<-5.5 and currentChange>-6.5):
+                        sense.set_pixel(hourIndex,2,red)
+                    #Row 3
+                    elif (currentChange<-6.5 and currentChange>-7.5):
+                        sense.set_pixel(hourIndex,3,red)
+                    #Row 4
+                    elif (currentChange<-7.5 and currentChange>-8.5):
+                        sense.set_pixel(hourIndex,4,red)
+                    #Row 5
+                    elif (currentChange<-8.5 and currentChange>-9.5):
+                        sense.set_pixel(hourIndex,5,red)
+                    #Row 6
+                    elif (currentChange<-9.5 and currentChange>-10.5):
+                        sense.set_pixel(hourIndex,6,red)
+                    #Row 7
+                    elif (currentChange<-10.5 and currentChange>-11.5):
+                        sense.set_pixel(hourIndex,7,red)
+                else:
+                    print (currentChange)
+                    sense.show_message(str(currentChange))
+            #For when the final displayed value is beyond 11.5% in either direction
+            else:
+                print (finalDisplayedChange)
+                sense.show_message(str(finalDisplayedChange))
 
-portCalc(times[0])
+            hourIndex += 1
+                    
+lightChange()
 
